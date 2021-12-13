091f2934f09b652c17ef2ce91d3d78bd272b4a4d
ariedel-cern <anton.riedel@cern.ch>
2021-12-13 11:41:43 +0100

    Feat: convert to new config format

---
+++ KinematicWeights/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File              : run.sh
+# Author            : Anton Riedel <anton.riedel@tum.de>
+# Date              : 04.12.2021
+# Last Modified Date: 06.12.2021
+# Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+
+aliroot -q -l -b run.C\(\"config.json\"\)
+
+exit 0
